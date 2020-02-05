4cbce1558cfe92c8adef8b6791555fde4a74144b
gdesaboyina <gopidesaboyina@gmail.com>
2020-02-04 19:03:09 -0500

    Update for_venkat_2.sh

--- for_venkat_2.sh
+++ for_venkat_2.sh
@@ -4,7 +4,7 @@
 sed -i "s;$;     filename.txt;g" filename.txt
 
 # appending filename at end of each file in current dir
-for filename in *.txt ; do 
-   sed -i "s;$;     $filename;g" $i 
+for filename in `ls *.txt` ; do 
+   sed -i "s;$;     $filename;g" $filename
 done
 
